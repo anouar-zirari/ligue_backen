154d864467690677753d46694da9f5fb225548de
unknown <anouaranwar70@gmail.com>
2021-05-11 19:44:19 +0200

    mysql configaration

---
+++ src/main/resources/player.sql
@@ -0,0 +1,7 @@
+CREATE TABLE person(
+    id INTEGER Not NULL AUTO_INCREMENT,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    shirt_number INT NOT NULL,
+    PRIMARY KEY(id)
+);
