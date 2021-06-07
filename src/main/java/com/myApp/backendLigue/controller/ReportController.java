8d75b365948910a2bf2d95438dc2843476a09300
unknown <anouaranwar70@gmail.com>
2021-06-07 22:43:17 +0200

    save a report work

--- src/main/java/com/myApp/backendLigue/controller/ReportController.java
+++ src/main/java/com/myApp/backendLigue/controller/ReportController.java
@@ -1,13 +1,25 @@
 package com.myApp.backendLigue.controller;
 
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.myApp.backendLigue.entity.Report;
+import com.myApp.backendLigue.service.ReportService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:4200")
 
 @RestController
 @RequestMapping("/report")
 public class ReportController {
+
+    @Autowired
+    private ReportService reportService;
+
+    @PostMapping("/save")
+    public void addReport(@RequestBody Report report){
+        this.reportService.addReport(report.getRefereeId(), report.getPlayerId(),
+                report.getGameId(), report.getReportTxt());
+    }
+
+
 }
